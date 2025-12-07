52cf1c6c40ff1a03dc890b0b1d069b53c8f2bc01
Ronald Camargo <ronaldcamargodev@gmail.com>
2025-12-07 18:07:24 -0400

    Refactor analog clock layout and styles; enhance digital display functionality

---
+++ README.md
@@ -0,0 +1,18 @@
+# Relógio Analógico
+
+![preview](./preview/preview.png)
+
+Projeto realizado por mim.
+
+[🔗 Clique aqui para acessar a página](https://ronald-ca.github.io/Clock/)
+
+## 📌 Tecnologias
+
+- HTML
+- CSS
+- JS
+- Git e GitHub
+
+## 📫 Contato
+
+ronaldcamargodev@gmail.com
--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-let digitalElement = document.querySelector('.digital')
-let sElement = document.querySelector('.p_s')
-let mElement = document.querySelector('.p_m')
-let hElement = document.querySelector('.p_h')
+const digitalTime = document.querySelector('.digital .time')
+const digitalDate = document.querySelector('.digital .date')
+const sElement = document.querySelector('.p_s')
+const mElement = document.querySelector('.p_m')
+const hElement = document.querySelector('.p_h')
+const numbersContainer = document.querySelector('.numeros')
 
-function updateClock(){
-    let now = new Date()
+for (let i = 1; i <= 12; i += 1) {
+    const number = document.createElement('div')
+    number.className = 'numero'
+    number.textContent = i
+    const angle = i * 30 - 90
+    number.style.setProperty('--angle', `${angle}deg`)
+    numbersContainer.appendChild(number)
+}
 
-    let hour = now.getHours()
-    let minute = now.getMinutes()
-    let second = now.getSeconds()
+const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
+    weekday: 'long',
+    day: '2-digit',
+    month: 'long'
+})
 
-    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`
+function updateClock() {
+    const now = new Date()
+    const hour = now.getHours()
+    const minute = now.getMinutes()
+    const second = now.getSeconds()
 
-    let sDeg = ( (360 / 60) * second ) - 90
-    let mDeg = ( (360 / 60 ) * minute ) - 90
-    let hDeg = ( (360 / 12 ) * hour) - 90
+    digitalTime.textContent = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`
+    digitalDate.textContent = dateFormatter.format(now)
+
+    const sDeg = (360 / 60) * second - 90
+    const mDeg = (360 / 60) * minute + (6 / 60) * second - 90
+    const hDeg = (360 / 12) * hour + (30 / 60) * minute + (30 / 3600) * second - 90
 
     sElement.style.transform = `rotate(${sDeg}deg)`
     mElement.style.transform = `rotate(${mDeg}deg)`
     hElement.style.transform = `rotate(${hDeg}deg)`
-
 }
 
-function fixZero(time){
-   return time < 10 ? `0${time}` : time
+function fixZero(time) {
+    return time < 10 ? `0${time}` : time
 }
 
 setInterval(updateClock, 1000)
